968e7511fa9d0d3c7b3cd58f782f61573984b8eb
Vikash Balasubramanian <v9balasu@uwaterloo.ca>
2024-06-10 20:21:47 -0400

    Initial commit

---
+++ rye-mode.el
@@ -0,0 +1,51 @@
+;;; rye-mode.el --- Python Version and Package Management with rye  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2024  vikigenius
+
+;; Author: Vikash Balasubramanian <v9balasu@uwaterloo.ca>
+;; Maintainer: Vikash Balasubramanian <v9balasu@uwaterloo.ca>
+;; URL: https://github.com/vikigenius/rye-mode
+;; Version: 0.1.0
+;; Package-Requires: ((emacs "26.1"))
+;; Keywords: tools
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;
+;; Python Version and Package Management in Emacs with rye
+;;
+
+;;; Code:
+
+(defgroup rye nil
+  "Rye integration with python mode."
+  :prefix "rye-"
+  :group 'tools)
+
+;;; TODO Handle if the version string does not match
+;;;###autoload
+(defun rye-get-python-version ()
+  "Get python version."
+  (when-let (root-path (projectile-locate-dominating-file default-directory ".python-version"))
+    (let* ((file-path (expand-file-name ".python-version" root-path))
+           (version
+            (with-temp-buffer
+              (insert-file-contents-literally file-path)
+              (string-trim (buffer-string)))))))
+
+  (provide 'rye-mode)
+;;; rye-mode.el ends here
